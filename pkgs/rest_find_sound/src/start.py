9c6dcab315d438a12a76adf22e5b16364b6b64b3
MiglyA <zq6295migly@gmail.com>
2019-06-23 10:53:58 +0900

    [add]hotword追加

--- pkgs/rest_find_sound/src/start.py
+++ pkgs/rest_find_sound/src/start.py
@@ -3,6 +3,7 @@
 # Restaurant 人に呼びかけられて角度を取得して発話
 
 import rospy
+from sound_system.srv import HotwordService
 from std_msgs.msg import String, Bool
 import time
 import datetime
@@ -10,6 +11,7 @@ import difflib
 import os
 import subprocess
 import getting_array
+
 
 class Restaurant_find_sound:
 	# 発話文のログファイル書き込みの関数
@@ -20,32 +22,39 @@ class Restaurant_find_sound:
 		else:
 			with open(self.log_file_name, "w") as f:
 				f.write(str(datetime.datetime.now()) + "\t" + "robot spoke:" + sentence + "\n")
+
 	# 声がしたメッセージを受け取ったら、角度を取得し、発話し、角度の情報を送る
 	def find_sound(self, data):
+		# Hotword 検出処理
+		rospy.wait_for_service("/hotword/detect", timeout=1)
+		print "hotword待機"
+		rospy.ServiceProxy("/hotword/detect", HotwordService)()
+
 		angle = getting_array.read('DOAANGLE')
-		subprocess.call('amixer sset Master 85% -q --quiet', shell=True) # 大声
+		subprocess.call('amixer sset Master 85% -q --quiet', shell=True)  # 大声
 		sentence = "I will go"
 		self.log_file_spoke(sentence)
 		rospy.loginfo("robot spoke: %s", sentence)
 		# ビープ音のディレクトリの絶対パスを変数に設定
 		beep_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'beep')
-		#PATH_beep_start = os.path.join(beep_path, 'start.wav')
-		#PATH_beep_stop = os.path.join(beep_path, 'stop.wav')
-		speech_wave =os.path.join(beep_path, 'speech.wav')
-		#subprocess.call('aplay -q --quiet {}'.format(PATH_beep_stop), shell=True)
+		# PATH_beep_start = os.path.join(beep_path, 'start.wav')
+		# PATH_beep_stop = os.path.join(beep_path, 'stop.wav')
+		speech_wave = os.path.join(beep_path, 'speech.wav')
+		# subprocess.call('aplay -q --quiet {}'.format(PATH_beep_stop), shell=True)
 		subprocess.call(['pico2wave', '-w={}'.format(speech_wave), sentence])
 		subprocess.call('aplay -q --quiet {}'.format(speech_wave), shell=True)
-		subprocess.call('amixer sset Master 75% -q --quiet', shell=True) # 声の大きさを戻す
+		subprocess.call('amixer sset Master 75% -q --quiet', shell=True)  # 声の大きさを戻す
 		self.pub.publish(str(angle))
 		print(str(angle))
-
 
 	def __init__(self):
 		rospy.init_node('rest_find_sound_start', anonymous=True)
 		rospy.Subscriber('rest_find_sound/find_sound', String, self.find_sound)
-		self.pub = rospy.Publisher('rest_find_sound/go_to_customer', String, queue_size=10) # 制御に客まで行くメッセージを投げる
-		self.log_file_flag = False # ログファイルを書き込みか追加か判定する
-		self.log_file_name = "{}/log{}.txt".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), "log"), datetime.datetime.now())
+		self.pub = rospy.Publisher('rest_find_sound/go_to_customer', String, queue_size=10)  # 制御に客まで行くメッセージを投げる
+		self.log_file_flag = False  # ログファイルを書き込みか追加か判定する
+		self.log_file_name = "{}/log{}.txt".format(os.path.join(os.path.dirname(os.path.abspath(__file__)), "log"),
+												   datetime.datetime.now())
+
 
 if __name__ == '__main__':
 	Restaurant_find_sound()
