56876bf3c861c34b3735b2812d997fb9b3fc0307
MiglyA <zq6295migly@gmail.com>
2019-06-26 14:49:21 +0900

    [add]rest_call_ducker実装

---
+++ pkgs/rest_call_ducker/scripts/rest_call_ducker.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from std_msgs.msg import String, Bool
+from rest_start_node.msg import Activate
+
+
+class Rest_Call_Ducker:
+	def __init__(self):
+		rospy.init_node("rest_call_ducker")
+		rospy.Subscriber("/call_ducker/finish", Bool, self.finish_callback)
+		self.call_ducker_pub = rospy.Publisher("/call_ducker/control", String, queue_size=10)
+		self.activate_pub = rospy.Publisher("/restaurant/activate", Activate, queue_size=10)
+
+	def activate_callback(self, msg):
+		# type: (Activate) -> None
+		if msg.id == 0:
+			# call_duckerにメッセージを送信
+			self.call_ducker_pub.publish("start")
+
+	def finish_callback(self, msg):
+		# type:(Bool) -> None
+		if msg.data:
+			activate = Activate()
+			activate.id = 1
+			self.activate_pub.publish(activate)
+
+
+if __name__ == '__main__':
+	Rest_Call_Ducker()
+	rospy.spin()
