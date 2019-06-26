30476e3983fb6067fd6c15d6e1b1a9732894eea3
Nenetti <akinori1090@gmail.com>
2019-06-26 15:12:06 +0900

    [fix]subscrive追加し忘れていたのを修正

--- pkgs/rest_call_ducker/scripts/rest_call_ducker.py
+++ pkgs/rest_call_ducker/scripts/rest_call_ducker.py
@@ -10,12 +10,14 @@ class Rest_Call_Ducker:
 	def __init__(self):
 		rospy.init_node("rest_call_ducker")
 		rospy.Subscriber("/call_ducker/finish", Bool, self.finish_callback)
+		rospy.Subscriber("/restaurant/activate", Activate, self.activate_callback)
 		self.call_ducker_pub = rospy.Publisher("/call_ducker/control", String, queue_size=10)
 		self.activate_pub = rospy.Publisher("/restaurant/activate", Activate, queue_size=10)
 
 	def activate_callback(self, msg):
 		# type: (Activate) -> None
 		if msg.id == 0:
+			print "call_ducker"
 			# call_duckerにメッセージを送信
 			self.call_ducker_pub.publish("start")
 
