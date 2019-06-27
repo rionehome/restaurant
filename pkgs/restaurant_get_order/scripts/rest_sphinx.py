9db3fff240d3156aafcf1e6a9a42971a92046e9a
MiglyA <zq6295migly@gmail.com>
2019-06-27 19:27:33 +0900

    [fix]関数の整理

--- pkgs/restaurant_get_order/scripts/rest_sphinx.py
+++ pkgs/restaurant_get_order/scripts/rest_sphinx.py
@@ -3,62 +3,60 @@
 
 import rospy
 from std_msgs.msg import String, Bool
-from sound_system.srv import HotwordService, NLPService
-from restaurant_get_order.msg import Order
+from sound_system.srv import HotwordService, NLPService, StringService
 from rest_start_node.msg import Activate
 import get_order
 import time
 
-order = Order()
-start_flag = False
-PLACE = "start_position"
-navigation_wait = False
-
 
 class RestGetOrder:
-	def __init__(self):
-		rospy.init_node('restaurant_get_order')
-		self.start_resume = rospy.Publisher('restaurant_getO/recognition_start', Bool, queue_size=10)
-		self.yes_no = rospy.Publisher('yes_no/recognition_start', Bool, queue_size=10)
-		self.speak = rospy.Publisher('/restaurant_nlp/speak', String, queue_size=10)  # 発話開始
+	def __init__(self, activate_id):
+		rospy.init_node("restaurant_get_order")
+		self.start_resume = rospy.Publisher("restaurant_getO/recognition_start", Bool, queue_size=10)
+		self.yes_no = rospy.Publisher("yes_no/recognition_start", Bool, queue_size=10)
+		self.speak = rospy.Publisher("/restaurant_nlp/speak", String, queue_size=10)  # 発話開始
 		self.activate_pub = rospy.Publisher("/restaurant/activate", Activate, queue_size=10)
 
-		rospy.Subscriber('/restaurant/activate', Activate, self.start_restaurant)  # 起動用
-		rospy.Subscriber('yes_no/recognition_result', String, self.get_yesno)  # yes_no
-		rospy.Subscriber('restaurant_getO/recognition_result', String, self.get_txt)  # 音声認識結果
-		rospy.Subscriber('restaurant_nlp/finish_speaking', Bool, self.finish_speaking)  # 発話終了
-		rospy.Subscriber('/navigation/goal', Bool, self.talk_order)
+		rospy.Subscriber("/restaurant/activate", Activate, self.start_restaurant)  # 起動用
+		rospy.Subscriber("yes_no/recognition_result", String, self.get_yesno)  # yes_no
+		rospy.Subscriber("restaurant_getO/recognition_result", String, self.get_txt)  # 音声認識結果
+		rospy.Subscriber("restaurant_nlp/finish_speaking", Bool, self.finish_speaking)  # 発話終了
+		rospy.Subscriber("/navigation/goal", Bool, self.talk_order)
+
+		self.id = activate_id
+		self.txt = ""
+		self.word_list = []
+		self.place = "start_position"
+		self.speak_topic = "/sound_system/speak"
+		self.activate_flag = False
 
 	# 処理の開始
 	def start_restaurant(self, data):
+		# type: (Activate) -> None
 		if data.id == 1:
-			global start_flag
 			print "rest_get_order"
-			start_flag = True
-			self.main()
+			self.activate_flag = True
+			self.table()
 
 	def send_place_msg(self, place):
-		global navigation_wait
+		# type: (str) -> None
 		# navigationに場所を伝える
-		rospy.wait_for_service('/sound_system/nlp', timeout=1)
-		response = rospy.ServiceProxy('/sound_system/nlp', NLPService)('Please go to {}'.format(place))
+		rospy.wait_for_service("/sound_system/nlp", timeout=1)
+		response = rospy.ServiceProxy("/sound_system/nlp", NLPService)('Please go to {}'.format(place))
 		print response.response
-		if "OK" in response.response:
-			navigation_wait = True
-		else:
+		if "OK" not in response.response:
 			# 次のノードに処理を渡す
 			activate = Activate()
-			activate.id = 2
+			activate.id = self.id + 1
 			self.activate_pub.publish(activate)
 
 	def start_speaking(self, sentence):
-		global finish_speaking_flag
-		finish_speaking_flag = False
-		if sentence != '':
-			self.speak.publish(sentence)
-			return
+		# type: (str) -> None
+		rospy.wait_for_service(self.speak_topic)
+		rospy.ServiceProxy(self.speak_topic, StringService)(sentence)
 
-	def finish_speaking(self, data):
+	@staticmethod
+	def finish_speaking(data):
 		global finish_speaking_flag
 		if data.data:
 			finish_speaking_flag = True
@@ -66,9 +64,8 @@ class RestGetOrder:
 
 	# 音声認識結果の取得
 	def get_txt(self, sentence):
-		global txt
-		if sentence != '':
-			txt = sentence.data
+		if sentence != "":
+			self.txt = sentence.data
 			return
 		time.sleep(1)
 		self.start_resume.publish(True)
@@ -76,7 +73,7 @@ class RestGetOrder:
 	# yes/noの音声認識結果の取得
 	def get_yesno(self, sentence):
 		global take_ans
-		if sentence != '':
+		if sentence != "":
 			take_ans = sentence.data
 			return
 		print('I\'m taking yes or no...')
@@ -84,123 +81,89 @@ class RestGetOrder:
 		self.yes_no.publish(True)
 
 	# キッチン到着後のオーダー復唱
-	def talk_order(self, data):
-		global take_ans, start_flag, PLACE, navigation_wait
-		if not start_flag:
-			navigation_wait = False
-			print PLACE
-			if PLACE == "table":
-				activate = Activate()
-				activate.id = 2
-				self.activate_pub.publish(activate)
-				start_flag = False
+	def talk_order(self):
+		if not self.activate_flag:
+			return
 
-			if PLACE == "kitchen":
+		if self.place == "table":
+			activate = Activate()
+			activate.id = self.id + 1
+			self.activate_pub.publish(activate)
+			self.activate_flag = False
+
+		if self.place == "kitchen":
+			self.kitchen()
+
+	def kitchen(self):
+		while True:
+			self.start_speaking('Order of Table A is')
+			# オーダーを列挙していく
+			for i in self.word_list:
+				self.start_speaking('{}'.format(i))
+			self.start_speaking('Is it OK?')
+			take_answer = ""
+			self.get_yesno("")
+			while not take_answer == 'yes' and not take_answer == 'no':
+				continue
+			self.yes_no.publish(False)
+			if take_answer == 'yes':
+				rospy.wait_for_service("/hotword/detect", timeout=1)
+				print "hotword待機"
+				rospy.ServiceProxy("/hotword/detect", HotwordService)()
+
+				self.start_speaking('Please put order on the tray')
 				while True:
-					self.start_speaking('Order of Table A is')
-					while not finish_speaking_flag:
-						continue
-					# オーダーを列挙していく
-					for i in word_list:
-						self.start_speaking('{}'.format(i))
-						while not finish_speaking_flag:
-							continue
-					self.start_speaking('Is it OK?')
-					while not finish_speaking_flag:
-						continue
-					take_ans = ''
-					self.get_yesno('')
-					while not take_ans == 'yes' and not take_ans == 'no':
+					time.sleep(5)  # 商品が置かれるまで5秒待機
+					self.start_speaking('Did you put order on the tray?')
+					self.get_yesno("")
+					while not take_answer == 'yes' and not take_answer == 'no':
 						continue
 					self.yes_no.publish(False)
-					if take_ans == 'yes':
-						rospy.wait_for_service("/hotword/detect", timeout=1)
-						print "hotword待機"
-						rospy.ServiceProxy("/hotword/detect", HotwordService)()
-
-						self.start_speaking('Please put order on the tray')
-						while not finish_speaking_flag:
-							continue
-						while True:
-							time.sleep(5)  # 商品が置かれるまで5秒待機
-							self.start_speaking('Did you put order on the tray?')
-							while not finish_speaking_flag:
-								continue
-							self.get_yesno('')
-							while not take_ans == 'yes' and not take_ans == 'no':
-								continue
-							self.yes_no.publish(False)
-							if take_ans == 'yes':
-								# 次への通信を書いてください
-								# 制御へ場所情報を送信.
-								PLACE = "table"
-								print "tableに移動するメッセージを投げる"
-								self.send_place_msg(PLACE)
-								return
-							else:
-								continue
-
-					# break
+					if take_answer == 'yes':
+						# 次への通信を書いてください
+						# 制御へ場所情報を送信.
+						self.place = "table"
+						print "tableに移動するメッセージを投げる"
+						self.send_place_msg(self.place)
+						return
 					else:
-						self.start_speaking('I say order again')
-						while not finish_speaking_flag:
-							continue
+						continue
+			else:
+				self.start_speaking('I say order again')
 
-	def main(self):
-		if start_flag:
-			global take_ans, start_flag, txt, finish_speaking_flag, word_list, PLACE
-			self.start_speaking('May I take your order?')
-			while not finish_speaking_flag:
+	def table(self):
+		self.start_speaking('May I take your order?')
+		while True:
+			self.txt = ""
+			self.get_txt("")
+			while self.txt == "":  # txt取得まで待機
 				continue
-			while True:
-				txt = ''
-				self.get_txt('')
-				while txt == '':  # txt取得まで待機
-					continue
 
-				take_ans = ''
-				word_list = []
-				word_list = get_order.main(txt.decode('utf-8'))
+			take_answer = ""
+			self.word_list = get_order.main(self.txt.decode('utf-8'))
 
-				self.start_speaking('Let me confirm your order')
-				while not finish_speaking_flag:
-					continue
+			self.start_speaking('Let me confirm your order')
 
-				for i in word_list:
-					# os.system("espeak '{}'".format(i))
-					self.start_speaking('{}'.format(i))
-					while not finish_speaking_flag:
-						continue
+			for i in self.word_list:
+				self.start_speaking('{}'.format(i))
 
-				self.start_speaking('Is it OK?')
-				while not finish_speaking_flag:
-					continue
+			self.start_speaking('Is it OK?')
 
-				self.get_yesno('')  # 聴きとった内容が正しいかを確認
-				while not take_ans == 'yes' and not take_ans == 'no':  # yesかnoを聞き取るまで待機
-					continue
-				self.yes_no.publish(False)
-				if take_ans == 'yes':
-					# os.system("espeak 'Sure'")
-					self.start_speaking('Sure')
-					while not finish_speaking_flag:
-						continue
-					order.order = word_list
-					# send_order(Order)
-					# start_flag = False
-					txt = ''
-					# 制御へ場所情報を送信.
-					PLACE = "kitchen"
-					self.send_place_msg(PLACE)
-					return
-				else:
-					self.start_speaking('Sorry, please say again your order')
-					while not finish_speaking_flag:
-						continue
-					# os.system("espeak 'Sorry, please say again your order'")
-					txt = ''
+			self.get_yesno("")  # 聴きとった内容が正しいかを確認
+			while not take_answer == 'yes' and not take_answer == 'no':  # yesかnoを聞き取るまで待機
+				continue
+
+			self.yes_no.publish(False)
+			if take_answer == 'yes':
+				self.start_speaking("Sure")
+				# 制御へ場所情報を送信.
+				self.place = "kitchen"
+				self.send_place_msg(self.place)
+				return
+			else:
+				self.start_speaking('Sorry, please say again your order')
 
 
 if __name__ == '__main__':
-	RestGetOrder()
+	RestGetOrder(1)
 	rospy.spin()
