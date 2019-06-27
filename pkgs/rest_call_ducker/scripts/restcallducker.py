e31a1c3dbaef8b811212b2cc8adab89de631c1ed
MiglyA <zq6295migly@gmail.com>
2019-06-28 00:18:01 +0900

    [fix]rest_call_duckerリファクタ終了

--- pkgs/rest_call_ducker/scripts/restcallducker.py
+++ pkgs/rest_call_ducker/scripts/restcallducker.py
@@ -6,17 +6,18 @@ from std_msgs.msg import String, Bool
 from rest_start_node.msg import Activate
 
 
-class Rest_Call_Ducker:
-	def __init__(self):
+class RestCallDucker:
+	def __init__(self, activate_id):
 		rospy.init_node("rest_call_ducker")
 		rospy.Subscriber("/call_ducker/finish", Bool, self.finish_callback)
 		rospy.Subscriber("/restaurant/activate", Activate, self.activate_callback)
 		self.call_ducker_pub = rospy.Publisher("/call_ducker/control", String, queue_size=10)
 		self.activate_pub = rospy.Publisher("/restaurant/activate", Activate, queue_size=10)
+		self.id = activate_id
 
 	def activate_callback(self, msg):
 		# type: (Activate) -> None
-		if msg.id == 0:
+		if msg.id == self.id:
 			print "call_ducker"
 			# call_duckerにメッセージを送信
 			self.call_ducker_pub.publish("start")
@@ -24,11 +25,14 @@ class Rest_Call_Ducker:
 	def finish_callback(self, msg):
 		# type:(Bool) -> None
 		if msg.data:
+			# tableの場所を記録
+			#
+			##
 			activate = Activate()
-			activate.id = 1
+			activate.id = self.id + 1
 			self.activate_pub.publish(activate)
 
 
 if __name__ == '__main__':
-	Rest_Call_Ducker()
+	RestCallDucker(0)
 	rospy.spin()
