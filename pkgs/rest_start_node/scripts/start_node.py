5d00312398b3b4f6ce9a58b797b0cc66a2061786
Nenetti <akinori1090@gmail.com>
2019-07-05 22:32:03 +1000

    [fix] serviceに対応させた

--- pkgs/rest_start_node/scripts/start_node.py
+++ pkgs/rest_start_node/scripts/start_node.py
@@ -7,6 +7,7 @@ from rest_start_node.msg import Activate
 from std_msgs.msg import Bool, String
 from location.srv import RegisterLocation
 
+
 @staticmethod
 def speak(sentence):
     # type: (str) -> None
@@ -18,12 +19,21 @@ def speak(sentence):
     rospy.wait_for_service("/sound_system/speak")
     rospy.ServiceProxy("/sound_system/speak", StringService)(sentence)
 
+
 if __name__ == '__main__':
     rospy.init_node("rest_start_node")
     pub = rospy.Publisher('/restaurant/activate', Activate, queue_size=10)
-    #---ここらか追加---
-    #客にHey Dackerを呼ばせるために、レストラン開始時に発話(いきなり発話しないように3秒待機へ変更)
+    # ---ここらか追加---
+    # 客にHey Dackerを呼ばせるために、レストラン開始時に発話(いきなり発話しないように3秒待機へ変更)
+
     time.sleep(3)
+
+    rospy.wait_for_service("/rest_judge_bar/detect")
+    direction = rospy.ServiceProxy("/rest_judge_bar/detect", StringService)()
+    speak("I am on the {} side".format(direction))
+
+    time.sleep(3)
+
     speak('Please call me Hey Ducker')
 
     rospy.wait_for_service("/navigation/register_current_location", timeout=1)
