40b346775636dac1cd91f21cb954e41004cda087
Nenetti <akinori1090@gmail.com>
2019-07-05 21:46:18 +1000

    [update] locationの場所登録を追加

--- pkgs/rest_start_node/scripts/start_node.py
+++ pkgs/rest_start_node/scripts/start_node.py
@@ -5,6 +5,7 @@ from sound_system.srv import NLPService, StringService, HotwordService
 import rospy
 from rest_start_node.msg import Activate
 from std_msgs.msg import Bool, String
+from location.srv import RegisterLocation
 
 @staticmethod
 def speak(sentence):
@@ -24,6 +25,10 @@ if __name__ == '__main__':
     #客にHey Dackerを呼ばせるために、レストラン開始時に発話(いきなり発話しないように3秒待機へ変更)
     time.sleep(3)
     speak('Please call me Hey Ducker')
+
+    rospy.wait_for_service("/navigation/register_current_location", timeout=1)
+    rospy.ServiceProxy("/navigation/register_current_location", RegisterLocation)("kitchen")
+
     activate = Activate()
     activate.id = 0
     pub.publish(activate)
