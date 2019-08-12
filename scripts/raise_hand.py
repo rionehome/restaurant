5c55dd4ae2289e2eb65f994eaadfafc19bd063bc
MiglyA <zq6295migly@gmail.com>
2019-08-12 11:16:26 +0900

    [add]効果音追加

--- scripts/raise_hand.py
+++ scripts/raise_hand.py
@@ -10,6 +10,7 @@ from nav_msgs.msg import Odometry
 from ros_posenet.msg import *
 from geometry_msgs.msg import Point, Quaternion
 from module.rviz_marker import RvizMarker
+from module.se import SE
 
 MARGIN = 0.8
 
@@ -23,6 +24,7 @@ class RaiseHand:
         self.flag = False
         self.marker = RvizMarker()
         self.raise_hand_persons = []
+        self.se = SE()
         
         rospy.init_node("raise_hand_human")
         rospy.Subscriber("/ros_posenet/result", Poses, self.pose_callback, queue_size=1)
@@ -30,98 +32,41 @@ class RaiseHand:
         self.client = actionlib.SimpleActionClient("/move_base", MoveBaseAction)
         self.raise_hand_position_pub = rospy.Publisher('/restaurant/raise_hand_position', String, queue_size=1)
     
-    def odometry_callback(self, msg):
-        # type: (Odometry)->None
-        """
-        位置情報の受け取り
-        :param msg:
-        :return:
-        """
-        self.sensor_x = msg.pose.pose.position.x
-        self.sensor_y = msg.pose.pose.position.y
+    def send_move_base(self, point):
+        # type:(tuple)->int
+        goal = MoveBaseGoal()
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.pose.position = Point(point[0], point[1], point[2])
+        goal.target_pose.pose.orientation = Quaternion(0, 0, 0, 1)
+        self.marker.register(goal.target_pose.pose)
+        self.client.wait_for_server()
+        print "move_baseに送信"
+        self.client.send_goal(goal)
+        self.client.wait_for_result()
+        return self.client.get_state()
     
-    def pose_callback(self, msgs):
-        # type: (Poses)->None
-        """
-        関節推定の結果を受け取り
-        :param msgs:
-        :return:
-        """
-        print self.flag
-        if self.flag:
-            return
-        
-        print
-        person_position = {}
-        for pose in msgs.poses:
-            
-            if not self.is_raise_hand(pose.keypoints):
-                continue
-            
-            try:
-                # 人間の三次元座標を計算
-                count = 0
-                sum_x = 0
-                sum_y = 0
-                sum_z = 0
-                for key in pose.keypoints:
-                    if key.part in self.point_list:
-                        sum_x += key.position.x
-                        sum_y += key.position.y
-                        sum_z += key.position.z
-                        count += 1
-                ave_x = sum_x / count
-                ave_y = sum_y / count
-                ave_z = sum_z / count
-                distance = math.sqrt(ave_x ** 2 + ave_y ** 2 + ave_z ** 2)
-                person_position.setdefault(distance, [ave_x, ave_y, ave_z])
-            except ZeroDivisionError:
-                print "countが0 @raise_hand"
-                return
-        
-        # 手を上げている一番近い人間
-        if len(person_position) == 0:
-            del self.raise_hand_persons[:]
-            return
-        print min(person_position), person_position[min(person_position)]
-        if min(person_position) < MARGIN:
-            print "人間が近すぎます"
-            del self.raise_hand_persons[:]
-            return
-        
-        safety_person_position = self.calc_safe_position(MARGIN, person_position[min(person_position)])
-        print safety_person_position
-        print len(self.raise_hand_persons)
-        if len(self.raise_hand_persons) < 10:
-            self.raise_hand_persons.append(safety_person_position)
-        else:
-            # 10のデータの平均を取る
+    def calc_average_position(self, pose):
+        try:
+            # 人間の三次元座標を計算
+            count = 0
             sum_x = 0
             sum_y = 0
-            for raise_hand_person in self.raise_hand_persons:
-                sum_x += raise_hand_person[0]
-                sum_y += raise_hand_person[1]
-            ave_x = sum_x / len(self.raise_hand_persons)
-            ave_y = sum_y / len(self.raise_hand_persons)
-            print ave_x, ave_y
-            
-            goal = MoveBaseGoal()
-            goal.target_pose.header.stamp = rospy.Time.now()
-            goal.target_pose.header.frame_id = "map"
-            goal.target_pose.pose.position = Point(self.raise_hand_persons[5][0], self.raise_hand_persons[5][1], 0)
-            goal.target_pose.pose.orientation = Quaternion(0, 0, 0, 1)
-            self.marker.register(goal.target_pose.pose)
-            self.client.wait_for_server()
-            self.client.send_goal(goal)
-            self.client.wait_for_result()
-            
-            if self.client.get_state() == actionlib.GoalStatus.SUCCEEDED:
-                print("SUCCEEDED")
-                print "到着"
-                self.flag = True
-            else:
-                del self.raise_hand_persons[:]
-                print"abourd"
+            sum_z = 0
+            for key in pose.keypoints:
+                if key.part in self.point_list:
+                    sum_x += key.position.x
+                    sum_y += key.position.y
+                    sum_z += key.position.z
+                    count += 1
+            ave_x = sum_x / count
+            ave_y = sum_y / count
+            ave_z = sum_z / count
+            distance = math.sqrt(ave_x ** 2 + ave_y ** 2 + ave_z ** 2)
+            return distance, [ave_x, ave_y, ave_z]
+        except ZeroDivisionError:
+            print "countが0 @raise_hand"
+        return None
     
     @staticmethod
     def is_raise_hand(keypoints):
@@ -163,6 +108,59 @@ class RaiseHand:
         safety_person_y = real_person_y
         
         return safety_person_x, safety_person_y
+    
+    def odometry_callback(self, msg):
+        # type: (Odometry)->None
+        """
+        位置情報の受け取り
+        :param msg:
+        :return:
+        """
+        self.sensor_x = msg.pose.pose.position.x
+        self.sensor_y = msg.pose.pose.position.y
+    
+    def pose_callback(self, msgs):
+        # type: (Poses)->None
+        """
+        関節推定の結果を受け取り
+        :param msgs:
+        :return:
+        """
+        if self.flag:
+            return
+        
+        person_position = {}
+        for pose in msgs.poses:
+            
+            if not self.is_raise_hand(pose.keypoints):
+                continue
+            
+            result = self.calc_average_position(pose)
+            if result == None:
+                continue
+            person_position.setdefault(result[0], result[1])
+        
+        # 手を上げている一番近い人間
+        if len(person_position) == 0:
+            del self.raise_hand_persons[:]
+            return
+        if min(person_position) < MARGIN:
+            print "人間が近すぎます"
+            del self.raise_hand_persons[:]
+            return
+        
+        safety_person_position = self.calc_safe_position(MARGIN, person_position[min(person_position)])
+        if len(self.raise_hand_persons) < 10:
+            self.raise_hand_persons.append(safety_person_position)
+            self.se.play(self.se.DISCOVERY)
+        else:
+            status = self.send_move_base((self.raise_hand_persons[5][0], self.raise_hand_persons[5][1], 0))
+            if status == actionlib.GoalStatus.SUCCEEDED:
+                print "到着"
+                self.flag = True
+            else:
+                del self.raise_hand_persons[:]
+                print"失敗"
 
 
 if __name__ == '__main__':
